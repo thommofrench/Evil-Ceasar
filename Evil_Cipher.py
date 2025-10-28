ca33191f84b89ed5fea44fd1d0b6de91cfaac08b
Tom <thommo.french@gmail.com>
2025-10-28 14:47:34 +1000

    Evil Serpent defines functions

--- Evil_Cipher.py
+++ Evil_Cipher.py
@@ -1,34 +1,31 @@
-a = input('Message: ')
+def key(key_phrase):
+    cipher_naughty_word = 0
+    # get a the key
+    for key in range(len(b)):
+        jawn = ord(b[key])
+        cipher_naughty_word += jawn
+    return cipher_naughty_word
+
+def encode(key, encode):
+    message = input('Message: ')
+    encoded = [''] * len(message)
+    if encode == 'e':
+        encode = 1
+    elif encode == 'd':
+        encode = -1
+    else:
+        print('Invalid option selected. Exiting...')
+        exit()
+    # encode the jawn
+    for enc in range(len(message)):
+        encoded[enc] = chr(ord(message[enc]) + encode*key)
+    return encoded
+
+x = input('Would you like to evil encode or decode a message? (e/d): ')
 b = input('Key: ')
 
-e = [''] * len(a)
-d = e
-
-cipher_naughty_word = 0
-cipher_other_naughty_word = 0
-
-
-
 # get a the key
-for key in range(len(b)):
-    jawn = ord(b[key])
-    cipher_naughty_word += jawn
+evil_key = key(b)
+evil_message = encode(evil_key, x)
 
-# encode the jawn
-for enc in range(len(a)):
-    e[enc] = chr(ord(a[enc]) + cipher_naughty_word)
-
-print('Encoded Message: ' + ''.join(e))
-
-c = input('Key the Jawn: ')
-# get a the key
-for key2 in range(len(c)):
-    jawn2 = ord(c[key2])
-    cipher_other_naughty_word += jawn2
-
-# decode the jawn
-for dec in range(len(e)):
-    d[dec] = chr(ord(e[dec]) - cipher_other_naughty_word)
-
-
-print('Decoded Message: ' + ''.join(d))
+print('Evil Message: ' + ''.join(evil_message))
